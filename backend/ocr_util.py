b9aaeda5a09432b83e4313dee64200ce050a4f06
stevegodly <stevegodly12@gmail.com>
2024-06-28 08:33:32 +0530

    improved extraction

--- backend/ocr_util.py
+++ backend/ocr_util.py
@@ -5,6 +5,24 @@ import numpy as np
 
 reader = easyocr.Reader(['en','hi'])
 
+import easyocr
+import cv2
+import re
+import numpy as np
+from PIL import Image,ImageEnhance
+
+reader = easyocr.Reader(['en','hi'])
+
+def resize_image(input_image_path):
+    with Image.open(input_image_path) as img:
+        resized_img = img.resize((1040, 780), Image.ANTIALIAS)
+    return resized_img    
+
+def increase_brightness(img):
+    enhancer = ImageEnhance.Brightness(img)
+    brightened_img = enhancer.enhance(1.5)
+    return brightened_img
+
 def extract_english_text(mixed_text):
     english_pattern = r'[A-Za-z0-9\s.,!?/-]+'
 
@@ -15,9 +33,10 @@ def extract_english_text(mixed_text):
     return english_text
 
 def extract_text(path):
-    img=cv2.imread(path)
+    r_img=resize_image(path)
+    img=increase_brightness(r_img)
     print("fininshed ocr download")
-    result = reader.readtext(img, paragraph=True)
+    result = reader.readtext(img)
     print("finished reading")
     text=''
     for item in result:
@@ -40,7 +59,8 @@ def extract_english_pan(mixed_text):
     return english_text
 
 def extract_pan(path):
-    img=cv2.imread(path)
+    r_img=resize_image(path)
+    img=increase_brightness(r_img)
     print("fininshed ocr download")
     result = reader.readtext(img)
     print("finished reading")
